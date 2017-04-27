be6fa5a644e12b649e627889ca5b03825ac51f39
prevosis <aadilzbhatti@gmail.com>
2017-04-26 21:49:25 -0500

    jkdf

--- src/node/participant/participant.go
+++ src/node/participant/participant.go
@@ -20,6 +20,7 @@ var wg sync.WaitGroup
 func Start(hostname string, id int) error {
   log.Println("Starting participant")
   self = New(hostname, id)
+  log.Println(self)
   go self.setupRPC()
   wg.Add(1)
   wg.Wait()
