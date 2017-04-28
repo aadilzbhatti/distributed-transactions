51485b2574a5b0d817e5792ca75f5af12b50583f
prevosis <aadilzbhatti@gmail.com>
2017-04-28 10:11:52 -0500

    i think this will work

--- src/node/coordinator/rpcs.go
+++ src/node/coordinator/rpcs.go
@@ -114,10 +114,14 @@ func (c Coordinator) Commit(ca *CommitArgs, reply *bool) error {
 
 		var check bool
 		err = client.Call("Participant.CanCommit", &cca, &check)
-		if err != nil && err.Error() != "No such transaction in server" {
-			log.Println("Error in Commit/RPC:", err)
-			client.Close()
-			return err
+		if err != nil {
+			if err.Error() != "No such transaction in server" {
+				log.Println("Error in Commit/RPC:", err)
+				client.Close()
+				return err
+			} else {
+				continue
+			}
 		}
 
 		if !check {
