542bf9cfdf56e0920d71a5197ed79ad3e378fec2
prevosis <aadilzbhatti@gmail.com>
2017-04-29 16:37:10 -0500

    seeing more errors as I debug

--- src/node/participant/rpcs.go
+++ src/node/participant/rpcs.go
@@ -159,7 +159,7 @@ func (p *Participant) GetKey(ga *GetArgs, reply *string) error {
 		}
 	}
 
-	if v, ok := self.Objects[ga.Key]; ok {
+	if v, ok := self.Transactions[ga.Tid].updates[ga.Key]; ok {
 		if v.currTrans != ga.Tid && v.currTrans != 0 {
 			log.Println(v)
 			*reply = self.Transactions[ga.Tid].initial[ga.Key].Value
