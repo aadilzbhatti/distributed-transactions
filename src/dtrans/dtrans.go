92660414f3cd9fd49278babffe20931d21fd7df5
prevosis <aadilzbhatti@gmail.com>
2017-05-01 03:49:09 -0500

    yes

--- src/dtrans/dtrans.go
+++ src/dtrans/dtrans.go
@@ -77,7 +77,9 @@ func runCommand(cmds []string, i int) {
 		res, err := Get(cmds[i+1], cmds[i+2], currentId)
 		if err != nil {
 			fmt.Println("Could not get:", err)
-			currentId = 0
+      if err.Error() != "No such object in server" {
+        currentId = 0
+      }
 			return
 		}
 		if res != "" {
