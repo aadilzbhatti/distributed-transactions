0f4af768d6dd39d7e677d6b4c695a774e9f8c1a2
prevosis <aadilzbhatti@gmail.com>
2017-05-01 03:03:38 -0500

    yes

--- src/node/coordinator/graph.go
+++ src/node/coordinator/graph.go
@@ -64,6 +64,7 @@ func (g *Graph) AddEdge(u string, v string, trans int32) error {
 			g.nodes[v].neighbors[u] = v1
 			g.inUse[trans] = append(g.inUse[trans], &edge{v1, v2, trans})
 			fmt.Printf("%+v\n", g)
+			fmt.Printf("%+v, %+v\n", g.nodes[u], g.nodes[v])
 			return nil
 		} else {
 			fmt.Println("YOU FUCKED UP BOY", v)
